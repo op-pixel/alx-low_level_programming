12d5c2599606b60025cb7734bda323cf39bb51be
op-pixel <horpheoluhwah@gmail.com>
2023-03-18 13:01:55 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
- * main -4-puts.c
- * description - Write a C program that prints exactly
+ * main - this is the main function
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle"):
-  returns (0);
+puts("\"Programming is like building a multilingual puzzle):
+returns [0];
 }
