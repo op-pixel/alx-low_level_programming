b7ece31464ff18aede4485e09e16d1aa189f7180
op-pixel <horpheoluhwah@gmail.com>
2023-03-19 02:02:07 -0700

    hexidecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
+
 /**
 *main - Prints numbers between 0 to 9 and letters between a to f.
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int i;
+	char j;
 
-	for (i = 48; i < 58; i++)
-	putchar(i);
-}
-
-	for (i = 48; i < 58; i++)
-{
-	putchar(i);
-
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
-	}
+	for (i = 0; i < 10; i++)
+	  putchar(i + '0');
+	for (j = 'a'; j <= 'f'; j++)
+	  putchar(j);
 	putchar('\n');
 	return (0);
 }
