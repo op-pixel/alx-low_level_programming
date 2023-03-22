1ec7fe3c3aa01f526cb8e4889d2f869c030e45e0
op-pixel <horpheoluhwah@gmail.com>
2023-03-22 12:00:02 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(' + ');
+		_putchar(' , ');
+		_putchar(' ');
+		return (1);
+	
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		-putchar(',');
+		_putchar(' ');
+		return (0);}
+}
+else
+	{ putchar
+
+
+
+}
