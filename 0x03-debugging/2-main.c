f37db1f93d511a2a55448a6f1dc101d74840aba8
op-pixel <horpheoluhwah@gmail.com>
2023-03-23 06:55:03 -0700

    intergers

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -16,14 +15,18 @@ if (a > b && b > c)
 {
 largest = a;
 }
+else if (a > c && c > b)
+largest = a;
 else if (b > a && a > c)
-{
-largest = c;
-}
-else if (b > c)
 {
 largest = b;
 }
+else if (b > c && c > a)
+
+largest = b;
+else if (c > a && a > b)
+largest = c;
+
 else
 {
 largest = c;
